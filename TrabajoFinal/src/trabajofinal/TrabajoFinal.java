c291a7ca4a95c1517ef65646de9c63c042a5eb2a
mikecrackexe <christianpuica@gmail.com>
2026-03-26 16:41:17 -0500

    Add files via upload

---
+++ TrabajoFinal/src/trabajofinal/TrabajoFinal.java
@@ -0,0 +1,18 @@
+/**
+ *@author CHRISTIAN JESUS
+ * fecha de creacion:26/03/2026
+ * 1.Creacion de la ventana grafica del login
+ * 2.Creacion de la conexion a la base de datos
+ */
+package trabajofinal;
+
+import trabajofinal.Vista.LoginPrincipal;
+
+public class TrabajoFinal {
+
+    public static void main(String[] args) {
+        LoginPrincipal login = new LoginPrincipal();
+        login.setVisible(true);
+    }
+    
+}
